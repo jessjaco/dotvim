ecced65ddc935d0b264a0a00ca5359036a4b44af
Jesse Anderson <jesse.anderson@gmail.com>
2025-10-24 21:31:04 -0500

    tweak yaml support

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
+-- Set these before loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
